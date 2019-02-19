daff588a8cdf47cee1722fab0d03fc3bcbbca7a0
Lucas Fontes <lfonteesc@gmail.com>
2019-02-19 11:00:49 -0300

    Working

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-
+import * as BooksAPI from './BooksAPI'
 
 class SearchBooks extends Component {
 
+    state = {
+        findBooks: [],
+        query: ''
+        }
+
+
+    handleSearch(e) {
+        if (e.target.value !== "") {
+          this.setState({ query: e.target.value });
+          BooksAPI.search(this.state.query).then(findBooks => {
+            this.setState({ findBooks: !findBooks || findBooks.error ? [] : findBooks });
+          });
+        } else {
+          this.setState({ findBooks: [] });
+        }
+      }
+
     render(){
+       
+        const { query } = this.state
+        const { books } = this.props
+        const a = BooksAPI.search(query).then()
+        console.log(a)
+       
         return (
             <div>
                 <div className="search-books">
@@ -14,20 +37,23 @@ class SearchBooks extends Component {
                     <button className="close-search">Close</button>
                 </Link>
                     <div className="search-books-input-wrapper">
-                    {/*
-                        NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                        You can find these search terms here:
-                        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-    
-                        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                        you don't find a specific author or title. Every search is limited by search terms.
-                    */}
-                    <input type="text" placeholder="Search by title or author"/>
-    
+                    <input type="text" placeholder="Search by title or author"
+                        onChange={this.handleSearch.bind(this)}
+                    />
                     </div>
                 </div>
+
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <ol className="books-grid">
+                        {this.state.findBooks.map(book => (
+                             <li className="book-cover" 
+                             style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")`
+                             }}></li>
+
+                    ))}
+                    </ol>
+                  
+                    
                 </div>
                 </div>
             </div>
